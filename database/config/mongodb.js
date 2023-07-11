d85ef799505fbca940fd8cf362169027cd8c29c5
nikhil01010 <crimsondawn0110@gmail.com>
2023-07-11 19:55:49 +0530

    completed

---
+++ database/config/mongodb.js
@@ -0,0 +1,10 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://user1:0000@cluster0.r9pzcfi.mongodb.net/?retryWrites=true&w=majority'
+
+async function getEmpData() {
+    let client = await MongoClient.connect(url);
+    let connection = client.db('issueTracker'); // Establish connection
+    return connection.collection('IssueTracker2023'); // creating a collection and naming it also
+}
+
+module.exports = getEmpData;
